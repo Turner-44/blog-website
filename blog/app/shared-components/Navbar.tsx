6b778aeff1c2fa70e4f66ca335b843e052b11ea8
Matthew Turner <82361399+Turner-44@users.noreply.github.com>
2025-09-10 20:26:44 -0400

    Initial setup UI

---
+++ blog/app/shared-components/Navbar.tsx
@@ -0,0 +1,42 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+const containerClasses =
+    'prose prose-xl mx-auto flex flex-col items-center justify-between'
+const navLinks = [
+    { href: '/', label: 'Blog' },
+    { href: '/about', label: 'About' },
+    { href: '/contact', label: 'Contact' },
+]
+
+export default function Navbar() {
+    return (
+        <div>
+            <Image
+                src="/images/logo.png"
+                alt="Becoming Matthew Logo"
+                width={100}
+                height={100}
+                className={`${containerClasses} p-4`}
+                priority={true}
+            />
+            <nav>
+                <div className="bg-black/90 p-3 text-white sticky top-0 drop-shadow-md z-10">
+                    <div className={`${containerClasses}`}>
+                        <div className="flex flex-row justify-center sm:justify-evenly align-middle gap-4">
+                            {navLinks.map(({ href, label }) => (
+                                <Link
+                                    key={href}
+                                    href={href}
+                                    className="text-2xl text-white/85 font-bold no-underline hover:text-white"
+                                >
+                                    {label}
+                                </Link>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
