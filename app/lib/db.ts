c131827ac48bf3f2305e3fdf444349ae1f21bc4b
Matthew Turner <82361399+Turner-44@users.noreply.github.com>
2025-09-15 16:22:49 -0400

    Remove extra folder nesting

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,10 +1,5 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import {
-    DynamoDBDocumentClient,
-    GetCommand,
-    PutCommand,
-    UpdateCommand,
-} from '@aws-sdk/lib-dynamodb';
+import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 
 const dbClient = new DynamoDBClient({
     credentials: {
