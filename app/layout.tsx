0827d07542c113d72593d2216c672e9f109005d0
Matthew Turner <82361399+Turner-44@users.noreply.github.com>
2025-09-21 16:29:37 -0400

    Make footer stay at the bottom of the page

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,9 +16,9 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body>
+            <body className="min-h-screen flex flex-col">
                 <Navbar />
-                {children}
+                <main className="flex-1">{children}</main>
                 <Footer />
             </body>
         </html>
