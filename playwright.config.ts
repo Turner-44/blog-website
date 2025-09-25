e2dc3dd8fb550d5d563fd1b841f7d629505fd70c
Matthew Turner <82361399+Turner-44@users.noreply.github.com>
2025-09-25 19:33:52 -0400

    Wrong api url

--- playwright.config.ts
+++ playwright.config.ts
@@ -75,7 +75,7 @@ export default defineConfig({
     /* Run your local dev server before starting the tests */
     webServer: {
         command: 'npm run dev',
-        url: baseURL + '/heath',
+        url: baseURL + 'api/blogs/heath',
         reuseExistingServer: !process.env.CI,
     },
 });
