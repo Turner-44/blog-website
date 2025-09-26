8705ea2e9afbdab57e275bb12c14bdcc62f23bd3
Matthew Turner <82361399+Turner-44@users.noreply.github.com>
2025-09-25 23:18:08 -0400

    Default to .env file

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,8 +8,9 @@ import dotenv from 'dotenv';
 import path from 'path';
 dotenv.config({ path: path.resolve(__dirname, '.env') });
 
-const baseUrl =
-  process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000/farts';
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+if (!baseUrl) throw new Error('ENV File not setting variables');
 
 /**
  * See https://playwright.dev/docs/test-configuration.
