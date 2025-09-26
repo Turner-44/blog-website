55e5af8e4f32daa05f2258d7c673b33290f96394
Matthew Turner <82361399+Turner-44@users.noreply.github.com>
2025-09-25 21:21:39 -0400

    Building and starting before tests run

--- tests/view-blogs.spec.ts
+++ tests/view-blogs.spec.ts
@@ -1,8 +1,8 @@
 import test, { expect } from '@playwright/test';
 
 test('View Blogs', async ({ page }) => {
-    await page.goto('/');
-    await expect(
-        await page.getByRole('heading', { name: 'This is $development.' })
-    ).toBeVisible();
+  await page.goto('/');
+  await expect(
+    await page.getByRole('heading', { name: 'This is TEST.' })
+  ).toBeVisible();
 });
