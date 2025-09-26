3e2b70173e465a895f16fa4190dc5eb26a55a8c8
Matthew Turner <82361399+Turner-44@users.noreply.github.com>
2025-09-26 00:37:46 -0400

    We have a winner

--- lib/api/blog/get/get-blogs.tsx
+++ lib/api/blog/get/get-blogs.tsx
@@ -1,12 +1,6 @@
 import { BlogMetaData } from '@/types/blog';
 
 export default async function getBlogs(limit: number = 30) {
-  console.log('🔍 NEXT_PUBLIC_BASE_URL:', process.env.NEXT_PUBLIC_BASE_URL);
-  console.log(
-    '🔍 Full URL:',
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/blogs?limit=${limit}`
-  );
-
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/blogs?limit=${limit}`
   );
