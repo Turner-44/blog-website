b11bae75a9920a978c45c865603f77031249d4e7
Matthew Turner <82361399+Turner-44@users.noreply.github.com>
2025-09-25 23:08:32 -0400

    Send it and hope for the best

--- lib/api/blog/get/get-blogs.tsx
+++ lib/api/blog/get/get-blogs.tsx
@@ -1,13 +1,19 @@
 import { BlogMetaData } from '@/types/blog';
 
 export default async function getBlogs(limit: number = 30) {
-    const res = await fetch(
-        `${process.env.NEXT_PUBLIC_BASE_URL}/api/blogs?limit=${limit}`
-    );
+  console.log('🔍 NEXT_PUBLIC_BASE_URL:', process.env.NEXT_PUBLIC_BASE_URL);
+  console.log(
+    '🔍 Full URL:',
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/blogs?limit=${limit}`
+  );
 
-    if (!res.ok) throw new Error('Failed to fetch blogs');
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/blogs?limit=${limit}`
+  );
 
-    const { items }: { items: BlogMetaData[] } = await res.json();
+  if (!res.ok) throw new Error('Failed to fetch blogs');
 
-    return items;
+  const { items }: { items: BlogMetaData[] } = await res.json();
+
+  return items;
 }
--- tests/view-blogs.spec.ts
+++ tests/view-blogs.spec.ts
@@ -1,7 +1,8 @@
 import test, { expect } from '@playwright/test';
 
-test('View Blogs', async ({ page }) => {
+test('View Blogs', async ({ page, baseURL }) => {
   await page.goto('/');
+  console.log(baseURL);
   await expect(
     await page.getByRole('heading', { name: "I'm Matthew Welcome to my" })
   ).toBeVisible();
