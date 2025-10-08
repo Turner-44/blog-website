372ddeecf42e1f494f7bf7c5424a2826dacb6d92
Matthew Turner <82361399+Turner-44@users.noreply.github.com>
2025-10-08 10:17:30 -0400

    Updated zod schema

--- utils/zod-schemas.ts
+++ utils/zod-schemas.ts
@@ -4,7 +4,7 @@ export const blogScheme = z.object({
   title: z
     .string()
     .min(3, 'Title must be at least 3 characters long')
-    .max(50, 'Title must be at most 50 characters long'),
+    .max(65, 'Title must be at most 65 characters long'),
   slug: z
     .string()
     .min(3, 'Slug must be at least 3 characters long')
@@ -12,7 +12,7 @@ export const blogScheme = z.object({
   summary: z
     .string()
     .min(10, 'Summary must be at least 10 characters long')
-    .max(200, 'Summary must be at most 200 characters long'),
+    .max(250, 'Summary must be at most 250 characters long'),
   markdown: z.string().min(10, 'Markdown must be at least 10 characters long'),
   featureImage: z.file().mime(['image/png', 'image/jpeg']),
   previewImage: z.file().mime(['image/png', 'image/jpeg']),
