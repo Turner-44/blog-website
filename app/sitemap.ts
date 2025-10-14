694a2537292db6158690695a1cb84d87ee508617
Matthew Turner <82361399+Turner-44@users.noreply.github.com>
2025-10-13 23:08:55 -0400

    Fix for failing pipeline build

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,12 +1,20 @@
 import type { MetadataRoute } from 'next';
-import { getBlogList } from '@/lib/api/blog/get-blogs';
 import { BlogPost } from '@/types/blog';
+import { dynamoDBClient, buildAllBlogsQuery } from '@/lib/api/aws/dynamo';
+import { QueryCommand } from '@aws-sdk/lib-dynamodb';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl =
-    process.env.NEXT_PUBLIC_SITE_URL || 'https://becomingmatthew.com';
-  const blogListResult = await getBlogList(100);
-  const blogs = (blogListResult?.blogPosts ?? []) as BlogPost[];
+    process.env.NEXT_PUBLIC_SITE_URL ?? 'https://becomingmatthew.com';
+
+  //Buiild was failing in the pipeline when referencing internal api, so querying DynamoDB directly here instead
+  const url = new URL(`${baseUrl}/api/blogs`);
+  url.searchParams.set('limit', '100');
+  const dynamodbRes = await dynamoDBClient.send(
+    new QueryCommand(buildAllBlogsQuery(url, undefined))
+  );
+
+  const blogs = (dynamodbRes.Items ?? []) as BlogPost[];
 
   const staticPages = ['/', '/about', '/contact', '/blog/library'].map(
     (path) => ({
