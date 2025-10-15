5031fcb00ca28073496d1850ab70be693a2f82cb
Matthew Turner <82361399+Turner-44@users.noreply.github.com>
2025-10-14 20:04:42 -0400

    Simplify to test

--- lib/auth/next-auth-options.tsx
+++ lib/auth/next-auth-options.tsx
@@ -3,33 +3,8 @@ import CredentialsProvider from 'next-auth/providers/credentials';
 import GoogleProvider from 'next-auth/providers/google';
 import { getAuthSecrets } from '../api/aws/secrets-manager';
 
-let cachedSecrets: {
-  GOOGLE_CLIENT_ID: string;
-  GOOGLE_CLIENT_SECRET: string;
-  NEXTAUTH_SECRET: string;
-} | null = null;
-
 export const authOptions = async (): Promise<NextAuthOptions> => {
-  let GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID as string;
-  let GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET as string;
-  let NEXTAUTH_SECRET = process.env.NEXTAUTH_SECRET as string;
-
-  if (!NEXTAUTH_SECRET) {
-    if (!cachedSecrets) {
-      try {
-        const secrets = await getAuthSecrets();
-        cachedSecrets = secrets;
-      } catch (err) {
-        console.error('Failed to fetch secrets', err);
-        throw err;
-      }
-      GOOGLE_CLIENT_ID = cachedSecrets.GOOGLE_CLIENT_ID as string;
-      GOOGLE_CLIENT_SECRET = cachedSecrets.GOOGLE_CLIENT_SECRET as string;
-      NEXTAUTH_SECRET = cachedSecrets.NEXTAUTH_SECRET as string;
-    } else {
-      console.log('Using cached secrets');
-    }
-  }
+  const secrets = await getAuthSecrets();
 
   return {
     providers: [
@@ -51,11 +26,11 @@ export const authOptions = async (): Promise<NextAuthOptions> => {
             },
           })
         : GoogleProvider({
-            clientId: GOOGLE_CLIENT_ID,
-            clientSecret: GOOGLE_CLIENT_SECRET,
+            clientId: secrets.GOOGLE_CLIENT_ID,
+            clientSecret: secrets.GOOGLE_CLIENT_SECRET,
           }),
     ],
-    secret: NEXTAUTH_SECRET,
+    secret: secrets.NEXTAUTH_SECRET,
     session: { strategy: 'jwt' },
     callbacks: {
       session: ({ session }) => {
